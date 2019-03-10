8f41a6be4fdf940a8dc493f02761e88c92e67e2d
Muhammad Faraz <muhammadfaraz840@gmail.com>
2019-03-10 08:12:24 +0500

    data saved.

---
+++ LabManagementProject/Student.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace LabManagementProject
+{
+    partial class FrmStudent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblfirstname = new System.Windows.Forms.Label();
+            this.btnaddstudent = new System.Windows.Forms.Button();
+            this.lbllastname = new System.Windows.Forms.Label();
+            this.lblcontact = new System.Windows.Forms.Label();
+            this.lblemail = new System.Windows.Forms.Label();
+            this.lblregistration = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.txtfirstname = new System.Windows.Forms.TextBox();
+            this.txtlastname = new System.Windows.Forms.TextBox();
+            this.txtcontact = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.txtregistration = new System.Windows.Forms.TextBox();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.dgvstudent = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvstudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblfirstname
+            // 
+            this.lblfirstname.AutoSize = true;
+            this.lblfirstname.Location = new System.Drawing.Point(28, 34);
+            this.lblfirstname.Name = "lblfirstname";
+            this.lblfirstname.Size = new System.Drawing.Size(54, 13);
+            this.lblfirstname.TabIndex = 0;
+            this.lblfirstname.Text = "FirstName";
+            // 
+            // btnaddstudent
+            // 
+            this.btnaddstudent.Location = new System.Drawing.Point(470, 219);
+            this.btnaddstudent.Name = "btnaddstudent";
+            this.btnaddstudent.Size = new System.Drawing.Size(75, 23);
+            this.btnaddstudent.TabIndex = 1;
+            this.btnaddstudent.Text = "Add Student";
+            this.btnaddstudent.UseVisualStyleBackColor = true;
+            this.btnaddstudent.Click += new System.EventHandler(this.btnaddstudent_Click);
+            // 
+            // lbllastname
+            // 
+            this.lbllastname.AutoSize = true;
+            this.lbllastname.Location = new System.Drawing.Point(28, 76);
+            this.lbllastname.Name = "lbllastname";
+            this.lbllastname.Size = new System.Drawing.Size(55, 13);
+            this.lbllastname.TabIndex = 2;
+            this.lbllastname.Text = "LastName";
+            // 
+            // lblcontact
+            // 
+            this.lblcontact.AutoSize = true;
+            this.lblcontact.Location = new System.Drawing.Point(28, 114);
+            this.lblcontact.Name = "lblcontact";
+            this.lblcontact.Size = new System.Drawing.Size(44, 13);
+            this.lblcontact.TabIndex = 3;
+            this.lblcontact.Text = "Contact";
+            // 
+            // lblemail
+            // 
+            this.lblemail.AutoSize = true;
+            this.lblemail.Location = new System.Drawing.Point(28, 152);
+            this.lblemail.Name = "lblemail";
+            this.lblemail.Size = new System.Drawing.Size(32, 13);
+            this.lblemail.TabIndex = 4;
+            this.lblemail.Text = "Email";
+            // 
+            // lblregistration
+            // 
+            this.lblregistration.AutoSize = true;
+            this.lblregistration.Location = new System.Drawing.Point(28, 187);
+            this.lblregistration.Name = "lblregistration";
+            this.lblregistration.Size = new System.Drawing.Size(103, 13);
+            this.lblregistration.TabIndex = 5;
+            this.lblregistration.Text = "Registration Number";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(28, 224);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(37, 13);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Status";
+            // 
+            // txtfirstname
+            // 
+            this.txtfirstname.Location = new System.Drawing.Point(137, 31);
+            this.txtfirstname.Name = "txtfirstname";
+            this.txtfirstname.Size = new System.Drawing.Size(100, 20);
+            this.txtfirstname.TabIndex = 8;
+            // 
+            // txtlastname
+            // 
+            this.txtlastname.Location = new System.Drawing.Point(137, 73);
+            this.txtlastname.Name = "txtlastname";
+            this.txtlastname.Size = new System.Drawing.Size(100, 20);
+            this.txtlastname.TabIndex = 9;
+            // 
+            // txtcontact
+            // 
+            this.txtcontact.Location = new System.Drawing.Point(137, 111);
+            this.txtcontact.Name = "txtcontact";
+            this.txtcontact.Size = new System.Drawing.Size(100, 20);
+            this.txtcontact.TabIndex = 10;
+            // 
+            // txtemail
+            // 
+            this.txtemail.Location = new System.Drawing.Point(137, 145);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(100, 20);
+            this.txtemail.TabIndex = 11;
+            // 
+            // txtregistration
+            // 
+            this.txtregistration.Location = new System.Drawing.Point(137, 180);
+            this.txtregistration.Name = "txtregistration";
+            this.txtregistration.Size = new System.Drawing.Size(100, 20);
+            this.txtregistration.TabIndex = 12;
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Items.AddRange(new object[] {
+            "0",
+            "1"});
+            this.cmbStatus.Location = new System.Drawing.Point(116, 216);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(121, 21);
+            this.cmbStatus.TabIndex = 13;
+            // 
+            // dgvstudent
+            // 
+            this.dgvstudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvstudent.Location = new System.Drawing.Point(65, 285);
+            this.dgvstudent.Name = "dgvstudent";
+            this.dgvstudent.Size = new System.Drawing.Size(422, 150);
+            this.dgvstudent.TabIndex = 14;
+            // 
+            // FrmStudent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(593, 463);
+            this.Controls.Add(this.dgvstudent);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.txtregistration);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.txtcontact);
+            this.Controls.Add(this.txtlastname);
+            this.Controls.Add(this.txtfirstname);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblregistration);
+            this.Controls.Add(this.lblemail);
+            this.Controls.Add(this.lblcontact);
+            this.Controls.Add(this.lbllastname);
+            this.Controls.Add(this.btnaddstudent);
+            this.Controls.Add(this.lblfirstname);
+            this.Name = "FrmStudent";
+            this.Text = "Student";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvstudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblfirstname;
+        private System.Windows.Forms.Button btnaddstudent;
+        private System.Windows.Forms.Label lbllastname;
+        private System.Windows.Forms.Label lblcontact;
+        private System.Windows.Forms.Label lblemail;
+        private System.Windows.Forms.Label lblregistration;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox txtfirstname;
+        private System.Windows.Forms.TextBox txtlastname;
+        private System.Windows.Forms.TextBox txtcontact;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.TextBox txtregistration;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.DataGridView dgvstudent;
+    }
+}
+
---
+++ LabManagementProject/Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LabManagementProject
+{
+    public partial class FrmStudent : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source = FARAZ; Initial Catalog = ProjectB; Integrated Security = True");
+        private SqlCommand cmd;
+        private SqlDataAdapter adpt;
+        private DataSet dt;
+     
+
+        public FrmStudent()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //DisplayData();
+
+
+
+        }
+
+        private void btnaddstudent_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+           // cmd = new SqlCommand("insert into Student values('"+txtfirstname.Text+"', '"+txtlastname.Text+"', '"+txtcontact.Text+"', '"+txtemail.Text+"', '"+txtregistration.Text+"', '"+cmbStatus.Text+"')", conn);
+            //cmd.ExecuteNonQuery();
+            
+           // MessageBox.Show("student data saved");
+            DisplayData();
+            conn.Close();
+
+            
+        }
+
+        public void DisplayData()
+        {
+           // conn.Open();
+            adpt = new SqlDataAdapter("select * from Student", conn);
+            dt = new DataSet();
+            adpt.Fill(dt);
+            dgvstudent.DataSource = dt;
+           // conn.Close();
+        }
+    }
+}
